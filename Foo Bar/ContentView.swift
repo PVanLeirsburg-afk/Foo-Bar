d43f2deefb42d4e0c85e79ab80da856d3a06cf73
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-20 17:57:51 -0500

    Added the appropriate color background for each numbr.

--- Foo Bar/ContentView.swift
+++ Foo Bar/ContentView.swift
@@ -10,34 +10,44 @@ import SwiftUI
 struct ContentView: View {
     @State private var counter = 0
     @State private var display = "0"
+    @State private var color = Color.black
     var body: some View {
-        VStack {
-            Text("\(display)")
-                .font(.system(size: 128))
-                .multilineTextAlignment(.center)
-                .preferredColorScheme(.dark)
-                .onTapGesture {
-                    counter += 1
-                    if counter % 3 != 0 && counter % 5 != 0 {
-                        display = String(counter)
-                    }
-                        else if counter % 3 == 0 && counter % 5 == 0 {
-                            display = "Foo Bar"
-                        }
+            VStack {
+                ZStack {
+                    color.ignoresSafeArea()
+                    Text("\(display)")
+                        .font(.system(size: 128))
+                        .multilineTextAlignment(.center)
+                        .preferredColorScheme(.dark)
+                        .onTapGesture {
+                            counter += 1
+                            if counter % 3 != 0 && counter % 5 != 0 {
+                                color = Color.black
+                                display = String(counter)
+                            }
+                            else if counter % 3 == 0 && counter % 5 == 0 {
+                                color = Color.red
+                                display = "Foo Bar"
+                            }
                             else if counter % 3 == 0 {
+                                color = Color.green
                                 display = "Foo"
                             }
-                                else  {
-                                    display = "Bar"
-                    }
+                            else  {
+                                color = Color.yellow
+                                display = "Bar"
+                            }
+                        }
                 }
             }
-                .onLongPressGesture {
-                    counter = 0
-                    display = "0"
-                }
+            .onLongPressGesture {
+                counter = 0
+                color = Color.black
+                display = "0"
+            }
         }
     }
+    
 
 
 #Preview {
