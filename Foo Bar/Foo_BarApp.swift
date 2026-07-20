8673123d24c6065f7026eab82c2eed9a56f980bc
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-20 17:24:33 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Pamela VanLeirsburg on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
