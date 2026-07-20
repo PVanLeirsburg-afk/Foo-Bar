d8f96a2604cfd110f74a8e968e11001f762384ea
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-20 17:49:43 -0500

    Created logic for text display

--- Foo Bar/ContentView.swift
+++ Foo Bar/ContentView.swift
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    @State private var display = "0"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(display)")
+                .font(.system(size: 128))
+                .multilineTextAlignment(.center)
+                .preferredColorScheme(.dark)
+                .onTapGesture {
+                    counter += 1
+                    if counter % 3 != 0 && counter % 5 != 0 {
+                        display = String(counter)
+                    }
+                        else if counter % 3 == 0 && counter % 5 == 0 {
+                            display = "Foo Bar"
+                        }
+                            else if counter % 3 == 0 {
+                                display = "Foo"
+                            }
+                                else  {
+                                    display = "Bar"
+                    }
+                }
+            }
+                .onLongPressGesture {
+                    counter = 0
+                    display = "0"
+                }
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
